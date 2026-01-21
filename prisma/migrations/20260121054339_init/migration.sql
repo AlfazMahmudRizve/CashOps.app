35b2d7769548b88ce05b4f71dabdd2cfe3faae73
AlfazMahmudRizve <a.m.rizve3047@gmail.com>
2026-01-21 14:58:42 +0600

    chore: remove sqlite migrations

--- prisma/migrations/20260121054339_init/migration.sql
+++
@@ -1,9 +0,0 @@
--- CreateTable
-CREATE TABLE "Transaction" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "amount" REAL NOT NULL,
-    "type" TEXT NOT NULL,
-    "category" TEXT NOT NULL,
-    "date" DATETIME NOT NULL,
-    "createdAt" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP
-);
